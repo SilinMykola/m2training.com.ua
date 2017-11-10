55936459116b69735f844d5234a2cabdadd3a2fc
NikolaiSilin <silin.nickolay@gmail.com>
2017-11-10 18:14:25 +0200

    last

--- app/code/Training/Test/Block/Product/View/Description.php
+++ app/code/Training/Test/Block/Product/View/Description.php
@@ -3,7 +3,7 @@ namespace Training\Test\Block\Product\View;
 
 class Description extends \Magento\Catalog\Block\Product\View\Description
 {
-    public function _beforeToHtml() {
+    protected function _beforeToHtml() {
 
         $this->setTemplate('Training_Test::description.phtml');
         //$originalBlock->getProduct()->setDescription('Test description');
